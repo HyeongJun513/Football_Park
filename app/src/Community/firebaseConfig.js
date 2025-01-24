5f3e8175334b01cc9b68ca854f87997a690a8b84
HyeongJun513 <135329512+HyeongJun513@users.noreply.github.com>
2025-01-24 17:04:37 +0900

    Update firebaseConfig.js

--- app/src/Community/firebaseConfig.js
+++ app/src/Community/firebaseConfig.js
@@ -3,14 +3,14 @@ import 'firebase/compat/database';  // Realtime Database 사용
 
 // Firebase 설정
 const firebaseConfig = {
-  apiKey: "AIzaSyDMXqyUZD6Pj0y0TLh43cgKyZTe4Jf7Xz4",
+  apiKey: "Key",
   authDomain: "football-park.firebaseapp.com",
   databaseURL: "https://football-park-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "football-park",
   storageBucket: "football-park.appspot.com",
   messagingSenderId: "279491831076",
-  appId: "1:279491831076:web:0a80e9b712a1e4515bffb1",
-  measurementId: "G-CVM6SJBN1C"
+  appId: "1:app:web:Id",
+  measurementId: "G-ID"
 };
 
 // Firebase 초기화
